4e11ab1f2e1d24b0f90d71d2e88fabae38b6d390
Night Star <jd3606192@gmail.com>
2023-05-03 13:32:49 -0700

    fixed the router

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,25 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import AdminLayout from "layouts/admin";
 import ClientLayout from "layouts/client";
 
 const App = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  let session = localStorage.getItem('gatewayagency');
+
+  useEffect(() => {
+    if(location.pathname.indexOf('admin') >= 0){
+      if(!session){
+        navigate('/login');
+      }
+    }
+    // eslint-disable-next-line
+  }, [location.pathname]);
+
   return (
     <Routes>
-      <Route path="admin/*" element={<AdminLayout />} />
+      {session && <Route path="admin/*" element={<AdminLayout />} />}
       <Route path="/*" element={<ClientLayout />} />
     </Routes>
   );
--- Client/src/components/navbar/index.jsx
+++ Client/src/components/navbar/index.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = (props) => {
   const { brandText } = props;
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    localStorage.removeItem("gatewayagency");
+    navigate("/login");
+  }
 
   return (
     <nav className="pt-5 top-4 z-40 flex flex-row flex-wrap items-center justify-between rounded-xl bg-white/10 p-2 backdrop-blur-xl dark:bg-[#0b14374d]">
@@ -15,6 +21,9 @@ const Navbar = (props) => {
             {brandText}
           </Link>
         </p>
+      </div>
+      <div>
+        <p onClick={() => {handleLogout()}} className="font-bold">SIGN OUT</p>
       </div>
     </nav>
   );
