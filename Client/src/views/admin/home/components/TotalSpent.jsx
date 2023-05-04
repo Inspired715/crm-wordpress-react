bef29c699f5d3f7c90bb3e05287bcd9f441ab50a
Night Star <jd3606192@gmail.com>
2023-05-04 02:26:29 -0700

    change the css of Chart

--- Client/src/views/admin/home/components/TotalSpent.jsx
+++ Client/src/views/admin/home/components/TotalSpent.jsx
@@ -8,7 +8,9 @@ import api_url from "constant";
 const TotalSpent = () => {
   const {token, email} = JSON.parse(localStorage.getItem('gatewayagency'));
   const [data, setData] = useState([]);
-  
+  let today = new Date();
+  today = today.toISOString().slice(0, 10);
+
   async function handleGetChartData(type){
     const account = { 
       email: email,
@@ -37,29 +39,55 @@ const TotalSpent = () => {
       <h4 className="mb-[10px] max-w-full text-xl font-bold text-black md:w-[64%] text-[30px] text-left md:leading-[42px] lg:w-[46%] xl:w-[85%] 2xl:w-[75%] 3xl:w-[52%]">
         HISOTRY
       </h4>
-      <div className="text-left">
-        <label className="bg-transparent text-black px-[10px] py-[5px] mx-[15px] rounded-[5px] font-hel" style={{border:'1px solid black',cursor:'grabbing'}}
-          onClick={() => handleGetChartData(0)}
-        >Week</label>
-        <label className="bg-transparent text-black px-[10px] py-[5px] mx-[15px] rounded-[5px] font-hel" style={{border:'1px solid black',cursor:'grabbing'}}
-          onClick={() => handleGetChartData(1)}
-        >Month</label>
+      <div className="bg-black rounded-[20px]">
+        <div className="text-left m-[20px]">
+          <label className="bg-transparent text-white px-[25px] py-[5px] mx-[30px] rounded-[5px] font-hel" style={{border:'1px solid white',cursor:'grabbing'}}
+            onClick={() => handleGetChartData(0)}
+          >Week</label>
+          <label className="bg-transparent text-white px-[25px] py-[5px] rounded-[5px] font-hel" style={{border:'1px solid white',cursor:'grabbing'}}
+            onClick={() => handleGetChartData(1)}
+          >Month</label>
+        </div>
+        <Chart
+          series={[
+            {
+              name: "Amount",
+              data: data?.map(item => item.price),
+            }
+          ]} 
+          options={{
+            colors:['#FFFFFF'],
+            markers: {
+              size: 5,
+              colors: ['#D4B65E']
+            },
+            stroke: { width: 2},
+            xaxis: {
+              categories: data?.map(data => (data.dates === today)?'Today':data.dates),
+              // labels: {
+              //   show: true,
+              //   rotate: -90,
+              //   rotateAlways: true,
+              //   style: {
+              //     colors: "#FFFFFF"
+              //   }
+              // },
+              labels: {
+                style: {
+                  colors: '#FFFFFF'
+                }
+              }
+            },
+            yaxis: {
+              labels: {
+                style: {
+                  colors: '#FFFFFF'
+                }
+              }
+            }
+          }}
+          type="line" width="100%"/>
       </div>
-      <Chart 
-        series={[
-          {
-            name: "Amount",
-            data: data?.map(item => item.price)
-          }
-        ]} 
-        options={{
-          colors: ['#000000'],
-          stroke: { width: 3, curve: 'smooth' },
-          xaxis: {
-            categories: data?.map(data => data.dates),
-          }
-        }}
-        type="line" width="100%"/>
     </Card>
   );
 };
